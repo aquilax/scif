89c0bc0697e0b4728d559ba7210143bb7dccf4a1
Evgeni Vasilev <aquilax@gmail.com>
2011-06-22 12:18:46 +0300

    Language

--- application/language/bulgarian/forum_lang.php
+++ application/language/bulgarian/forum_lang.php
@@ -1,6 +1,13 @@
 <?php
 
 $lang = array(
+  'Home' => 'Начало',
+  'Reply' => 'Отговор',
+  'Title' => 'Заглавие',
+  'Password' => 'Парола',
+  'edit' => 'редакция',
+  'New topic' => 'Нова тема',
+
   'To add picture type the url with <b>i</b> in front <b>ihttp://</b>' => 'За да вмъкнете изображение, напишете URL с <b>i</b> отпред <b>ihttp://</b>',
 );
 
