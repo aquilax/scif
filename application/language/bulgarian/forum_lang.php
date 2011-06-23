73e07fd2c962d7301f68ad8a523288d72a92f164
Evgeni Vasilev <aquilax@gmail.com>
2011-06-23 10:15:51 +0300

    Comma issue

--- application/language/bulgarian/forum_lang.php
+++ application/language/bulgarian/forum_lang.php
@@ -12,7 +12,7 @@ $lang = array(
   'To add picture type the url with <b>i</b> in front <b>ihttp://</b>' => 'За да вмъкнете изображение, напишете URL с <b>i</b> отпред <b>ihttp://</b>',
   'This forum is still empty. Please %s something.' => 'Този форум е все още празен. Моля %s нещо.',
   'write' => 'напишете',
-  'Post' => 'Публикувай';
+  'Post' => 'Публикувай',
 );
 
 ?>
