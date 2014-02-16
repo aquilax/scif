a7c818d34f23dfd190869af0adea8868ec501549
aquilax <aquilax@gmail.com>
2014-02-16 07:49:20 +0100

    Image Upload ?

--- application/views/layouts/main_tpl.php
+++ application/views/layouts/main_tpl.php
@@ -6,26 +6,24 @@
 <meta name="description" content="<?php echo q($descr)?>" />
 <link rel="stylesheet" type="text/css" media="screen" href="/css/sf4.css" />
 <link rel="alternate" type="application/rss+xml" title="<?php echo lang('Latest posts')?>" href="<?php echo site_url('feed/rss')?>" /> 
-<?php
-  if ($analytics){
-?>
-<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', '<?php echo $analytics;?>']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script><?
-  }
-  echo $header_text;
-?>
+<?php echo $header_text; ?>
 </head>
 <body itemscope="itemscope" itemtype="http://schema.org/WebPage">
 <?php
+  if ($analytics){
+?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', '<?php echo $analytics; ?>', '<?php echo $_SERVER['SERVER_NAME']?>');
+  ga('send', 'pageview');
+
+</script>
+<?php
+  }
 echo '<h1>'.anchor('/', $site_title).'</h1>';
 echo '<div id="forum">';
 if (isset($path) && count($path) > 1){
--- application/views/partials/post_form_tpl.php
+++ application/views/partials/post_form_tpl.php
@@ -10,6 +10,12 @@
   echo form_input('title', set_value('title', $post['title']), 'id="title" size="60"').'<br/>';
   echo '</td>';
   echo '</tr>';
+?>
+  <tr><td colspan="2">
+  <button onclick="document.querySelector('input').click()"><?php lang('Add Image'); ?></button></div>
+    <input style="visibility: collapse; width: 0px;" type="file" onchange="upload(this.files[0])">  
+  </td></tr> 
+<?php
   echo '<tr>';
   echo '<td colspan="2">';
   echo form_textarea(array('name' => 'body', 'value' => set_value('body', $post['body']), 'cols'=>'60', 'rows'=>'10'));
@@ -32,3 +38,26 @@
   echo form_close();
   echo '</div>';
 ?>
+<script>
+    // https://hacks.mozilla.org/2011/03/the-shortest-image-uploader-ever/
+    function upload(file) {
+        /* Is the file an image? */
+        if (!file || !file.type.match(/image.*/)) return;
+    
+        /* It is! */
+        document.body.className = "uploading";
+
+        /* Lets build a FormData object*/
+        var fd = new FormData(); // I wrote about it: https://hacks.mozilla.org/2011/01/how-to-develop-a-html5-image-uploader/
+        fd.append("image", file); // Append the file
+        var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
+        xhr.open("POST", "http://api.imgur.com/3/upload.json"); // Boooom!
+        xhr.onload = function() {
+            // Big win!
+            console.log(xhr.responseText);
+            //document.querySelector("#link").href = JSON.parse(xhr.responseText).upload.links.imgur_page;
+        }
+        /* And now, we send the formdata */
+        xhr.send(fd);
+    }
+</script> 
